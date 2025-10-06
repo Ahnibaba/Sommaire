a6d854517c82a799bfb3407885a10dba24317a8c
Ahnibaba <aniebietedet2025@gmail.com>
2025-10-06 10:18:41 +0100

    Beautiful summary slides

--- src/components/summaries/summary-viewer.tsx
+++ src/components/summaries/summary-viewer.tsx
@@ -1,92 +1,77 @@
 "use client"
 import { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Card } from "../ui/card";
 import NavigationControls from "./navigation-controls";
+import ProgressBar from "./progressBar";
+import { parseSection } from "../../../utils/summary-helper";
+import ContentSection from "./content-section";
 
 
-const parseSection = (section: string) => {
-  const [title, ...content] = section.split("\n")
-  const cleanTitle = title.startsWith("#")
-    ? title.substring(1).trim()
-    : title.trim()
-
-  const points: String[] = []
-  
-  let currentPoint = ""
-
-  content.forEach((line) => {
-    const trimmedLine = line.trim()
-    if (trimmedLine.startsWith("•")) {
-      if (currentPoint) points.push(currentPoint.trim())
-      currentPoint = trimmedLine
-    } else if (!trimmedLine) {
-      if (currentPoint) points.push(currentPoint.trim())
-      currentPoint = ""
-    } else {
-      currentPoint += " " + trimmedLine
-    }
-  })
-
-  if (currentPoint) points.push(currentPoint.trim())
-  return { 
-     title: cleanTitle, 
-     points: points.filter(
-      (point) => point && !point.startsWith("#") && !point.startsWith("[Choose")
-     )
-    }
+const SectionTitle = ({ title }: { title: string }) => {
+  return (
+    <div className="flex flex-col gap-2 mb-6 sticky top-0
+    pt-2 pb-4 bg-background/80 background-blur-xs z-10">
+      <h2 className="text-3xl lg:text-4xl font-bold text-center
+       flex items-center justify-center gap-2">{title}</h2>
+    </div>
+  )
 }
+
 
 export default function SummaryViewer({ summary }: { summary: string }) {
   const [currentSection, setCurrentSection] = useState(0)
-  
+
   const handleNext = () => {
     setCurrentSection((prev) => Math.min(prev + 1, sections.length - 1))
   }
+
   const handlePrevious = () => {
-    setCurrentSection((prev) =>Math.max(prev - 1, 0))
+    setCurrentSection((prev) => Math.max(prev - 1, 0))
   }
 
   const handleSectionSelect = (index: number) => {
     setCurrentSection(Math.min(Math.max(index, 0), sections.length - 1))
   }
-  
+
 
   const sections = summary
-     .split("\n# ")
-     .map((section) => section.trim())
-     .filter(Boolean)
-     .map(parseSection)
+    .split("\n# ")
+    .map((section) => section.trim())
+    .filter(Boolean)
+    .map(parseSection)
 
   console.log(sections);
-     
+
 
   return (
-       <Card
-         className="relative px-2 h-[500px] sm:h-[600px] lg:h-[700px]
+    <Card
+      className="relative px-2 h-[500px] sm:h-[600px] lg:h-[700px]
          w-full xl:w-[600px] overflow-hidden bg-linear-to-br from-background
-         via-background/95 to-rose-500/5 backdrop-blue-lg shadow-2xl rounded-3xl border border-rose-500/10"
-       >
-          <div className="h-full overflow-y-auto scrollbar-hide pt-12 sm:pt-16 pb-20 sm:pb-24">
-             <div className="px-4 sm:px-6">
-               <h2 className="">{sections[currentSection]?.title || ""}</h2>
-               <ul className="">
-                 {sections[currentSection]?.points.map((point, index) => (
-                  <li key={index}>{point}</li>
-                 ))}
-               </ul>
-             </div>
-          </div>
+         via-background/95 to-rose-500/5 backdrop-drop-lg shadow-2xl rounded-3xl border border-rose-500/10"
+    >
+      <ProgressBar sections={sections} currentSection={currentSection} />
+      <div className="h-full overflow-y-auto scrollbar-hide pt-12 sm:pt-16 pb-20 sm:pb-24">
+        <div className="px-4 sm:px-6">
+          <SectionTitle title={sections[currentSection]?.title || ""} />
+          <ContentSection
+            title={sections[currentSection]?.title || ""}
+            points={sections[currentSection]?.points || []}
+          />
+        </div>
+      </div>
 
-         <NavigationControls
-          currentSection ={currentSection}
-          totalSections = {sections.length}
-          onPrevious = {handlePrevious}
-          onNext = {handleNext}
-          onSectionSelect = {setCurrentSection}
-        />
-       </Card>
+      <NavigationControls
+        currentSection={currentSection}
+        totalSections={sections.length}
+        onPrevious={handlePrevious}
+        onNext={handleNext}
+        onSectionSelect={setCurrentSection}
+      />
+    </Card>
 
-        
-      
+
+
   )
 }
+
+
